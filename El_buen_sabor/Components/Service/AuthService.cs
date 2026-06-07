9b79833ed897262d452482f2075616105a858671
Mathias Cabrera <mathiasdesign1@gmail.com>
2026-06-07 14:50:54 -0300

    LogoutAsync agregado en AuthService

--- El_buen_sabor/Components/Service/AuthService.cs
+++ El_buen_sabor/Components/Service/AuthService.cs
@@ -1,4 +1,11 @@
 ﻿using El_buen_sabor.Components.Models;
+using static System.Net.WebRequestMethods;
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
 
 namespace El_buen_sabor.Components.Service
 {
@@ -21,6 +28,25 @@ namespace El_buen_sabor.Components.Service
                 return null;
 
             return await response.Content.ReadFromJsonAsync<LoginResponse>();
+        }
+        public async Task<bool> LogoutAsync(string token)
+        {
+            try
+            {
+                using var request = new HttpRequestMessage(HttpMethod.Post, $"{_http.BaseAddress}api/Auth/logout");
+
+                if (!string.IsNullOrEmpty(token))
+                {
+                    request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
+                }
+                using var response = await _http.SendAsync(request);
+                return response.IsSuccessStatusCode;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error de red en AuthService.LogoutAsync: {ex.Message}");
+                return false;
+            }
         }
     }
 }
